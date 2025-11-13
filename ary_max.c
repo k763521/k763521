608c1a71bd19e447cdf42a595155b441ddc5dbaa
k763521 <202530211@email.daelim.ac.kr>
2025-11-13 09:38:07 +0900

    daelim

---
+++ ary_max.c
@@ -0,0 +1,11 @@
+// 배열 요소의 최대 값을 구하고 출력(요소 값 입력)
+#include <studio.h>
+#include <stdlib.h>
+
+/*--- 요소 개수가 n인 배열 a의 요소의 최댓값 ---*/
+int maxof(const int foo[], int elem_cnt) {
+    int max = foo[0];           //최댓값
+    for (int i = 1; i < elem_cnt; i++) {
+        if (foo[i]>max )
+    }
+}
